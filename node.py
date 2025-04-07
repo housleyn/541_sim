d65f31ba3b49d3fbeb4414e8d61b6d34e2ef027d
housleyn <housleyn@byu.edu>
2025-04-07 17:06:17 -0600

    fixed pressure coeeficient corerction equations, and tested, tested pass, could use more testing if debugging needed.

--- node.py
+++ node.py
@@ -6,15 +6,23 @@ class Node():
         self.aN = None 
         self.aS = None 
         self.b = None 
-        self.P = None
+        self.p = None
         self.position = None 
 
-    def define_coefficients(self, De, Fe, Dw, Fw, Dn, Fn, Ds, Fs, Sc, Sp, dx, dy):
-        self.aE = De + max(-Fe,0)
-        self.aW = Dw + max(Fw,0)        
-        self.aN = Dn + max(-Fn,0)
-        self.aS = Ds + max(Fs,0)
-        self.b = Sc * dx* dy
-        self.aP = self.aW + self.aE + self.aN + self.aS - Sp*dx*dy 
+    def define_pressure_correction_coefficients(self, alphau, alphav,rho, dx, dy,
+                                                aP_u_E, aP_u_W,
+                                                aP_v_N, aP_v_S,
+                                                u_star_W, u_star_E,
+                                                v_star_S, v_star_N):
+        self.aE = rho * dy**2 * alphau / aP_u_E if aP_u_E else 0.0
+        self.aW = rho * dy**2 * alphau / aP_u_W if aP_u_W else 0.0
+        self.aN = rho * dx**2 * alphav / aP_v_N if aP_v_N else 0.0
+        self.aS = rho * dx**2 * alphav / aP_v_S if aP_v_S else 0.0
+
+        self.aP = self.aE + self.aW + self.aN + self.aS
+
+        self.b = rho * ((u_star_W - u_star_E) * dy + (v_star_S - v_star_N) * dx)
+
         
-        
+            
+            
--- tests/test_node.py
+++ tests/test_node.py
@@ -18,12 +18,24 @@ def test_node_class_initialized_correctly(node):
     assert node.b is None 
     assert node.P is None
 
-def test_define_coefficients(node):
-    node.define_coefficients(De=1, Fe=1, Dw=2, Fw=3, Dn=4, Fn=5, Ds=6, Fs=7, Sc=8, Sp=9, dx=.1, dy=.1)
-    assert node.aE == 1
-    assert node.aW == 5
-    assert node.aN == 4
-    assert node.aS == 13
-    assert isclose(node.b, 0.08, rel_tol=1e-9)
-    assert isclose(node.aP, 1 + 5 + 4 + 13 - 0.09, rel_tol=1e-9)
+def test_define_pressure_correction_coefficients(node):
+    rho = 1000
+    dx = 0.0125
+    dy = 0.0025
+    aP_u_E = .01415
+    aP_u_W = .01415
+    u_star_W = .01415
+    u_star_E = .01415
+    v_star_S = 1.0
+    v_star_N = 2.0
+
+    aP_v_N = 0.005
+    aP_v_S = 0.00625
+    alphau = .5
+    alphav = .5
+
+    node.define_pressure_correction_coefficients(rho, alphau, alphav,dx, dy, aP_u_W, aP_u_E, aP_v_N, aP_v_S,
+                                                  u_star_W, u_star_E, v_star_S, v_star_N)
+
+    assert isclose(node.aE, 0.2208480565371025)
     
